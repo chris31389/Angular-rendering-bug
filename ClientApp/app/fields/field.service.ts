1d0dce78c3fc0711123249d2109913c68f44628c
Chris Butler <chris.butler@draycir.com>
2018-06-12 18:13:47 +0100

    working without api calls

--- ClientApp/app/fields/field.service.ts
+++ ClientApp/app/fields/field.service.ts
@@ -1,27 +1,15 @@
-import { Injectable, Inject } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 import { Field } from "./field";
 import { Observable } from "rxjs";
+import { of } from "rxjs/observable/of";
 
 @Injectable()
 export class FieldService {
+  private fields = [new Field({ id: "field123", name: "bob", key: "key" })];
   constructor(
-    private readonly http: HttpClient,
-    @Inject("ORIGIN_URL") private readonly appUrl: string
   ) {}
 
   getAll = (workflowId: string): Observable<Array<Field>> => {
-    if (!workflowId) {
-      throw new Error("workflowId is required");
-    }
-    return this.http.get(this.getUrl(workflowId)).map(this.mapResponse);
+    return of(this.fields)
   };
-
-  private mapResponse = (response: any): Array<Field> =>
-    Array.isArray(response)
-      ? response.map<Field>(element => new Field(element))
-      : new Array<Field>();
-
-  private getUrl = (workflowId: string): string =>
-    `${this.appUrl}/api/v1/workflows/${workflowId}/fields`;
 }
--- ClientApp/app/item-list/item.service.ts
+++ ClientApp/app/item-list/item.service.ts
@@ -1,39 +1,20 @@
-import { Injectable, Inject } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 import { Workflow } from "./item";
 import { Observable } from "rxjs";
+import { of } from "rxjs/observable/of";
 
 @Injectable()
 export class WorkflowService {
-  private readonly url = `${this.appUrl}/api/v1/workflows`;
+  private listItems = [new Workflow({ id: "abc123", name: "bob" })];
 
   constructor(
-    private readonly http: HttpClient,
-    @Inject("ORIGIN_URL") private readonly appUrl: string
   ) {}
 
   get workflows(): Observable<Array<Workflow>> {
-    return this.http.get(this.url).map(this.mapResponse);
+    return of(this.listItems);
   }
 
   get approvalsAppUrl(): Observable<string>{  
-    return this.http.get<any>(`${this.url}/app-url`)
-        .map(json => json.url);
+    return of("some-app-url");
   }
-
-  remove = (workflowId: string): Observable<any> =>
-    this.http.delete(`${this.url}/${workflowId}`);
-
-  create = (workflowName: string): Observable<Workflow> =>
-    this.http
-      .post(this.url, { name: workflowName })
-      .map(json => new Workflow(json));
-
-  syncAll = (): Observable<Array<Workflow>> =>
-    this.http.post(this.url + "/sync", {}).map(this.mapResponse);
-
-  private mapResponse = (response: Workflow) =>
-    Array.isArray(response)
-      ? response.map<Workflow>(element => new Workflow(element))
-      : new Array<Workflow>();
 }
